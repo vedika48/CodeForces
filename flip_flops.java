316d60ba81865b8f35aac0aa75694524de9c3fba
Vedika Mali <vedika.mali85@gmail.com>
2026-04-20 20:41:00 +0530

    20/4/2026: 1

--- flip_flops.java
+++ flip_flops.java
@@ -1,42 +1,30 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class flip_flops {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        ArrayList<Integer> res = new ArrayList<>();
+        ArrayList<Long> res = new ArrayList<>();
         for(int i=0; i<t; i++){
             int n = sc.nextInt();
-            int c = sc.nextInt();
+            long c = sc.nextLong();
             int k = sc.nextInt();
-            Integer[] a = new Integer[n];
+            int[] a = new int[n];
             for(int j=0; j<n; j++){
                 a[j] = sc.nextInt();
             }
-            Arrays.sort(a, Collections.reverseOrder());
+            Arrays.sort(a);
             for(int j=0; j<n; j++){
-                if(a[j]>c) continue;
-                if(k==0) break;
-                if(c-a[j] < k){
-                    k -= c-a[j];
-                    a[j] = c;
-                }
-                else if(c-a[j] > k){
-                    a[j] += k;
-                    k = 0;
-                }
-            }
-            for(int j=a.length-1; j>=0; j--){
-                if(a[j] < c) continue;
-                c += a[j];
+                if(a[j]>c) break;
+                long o = Math.min((long) k, c - a[j]);
+                k -= o;
+                c += a[j] + o;
             }
             res.add(c);
         }
-        for(int val : res) System.out.println(val);
+        for(long val : res) System.out.println(val);
         sc.close();
     }
 }
